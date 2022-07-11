7c31cc60f331300e706bb416f7095e8211bcfef4
Лера Гордеева <valeri.gorde@mail.ru>
2022-07-11 21:58:56 +0300

    Добавьте файлы проекта.

---
+++ Unit_4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Unit_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите ваш любимый цвет");
+
+            string color = Console.ReadLine();
+
+            if (color == "красный") {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Ваш любимый цвет красный!");
+            }
+
+            else if (color == "зелёный") {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.ForegroundColor = ConsoleColor.Black;
+
+                Console.WriteLine("Ваш любимый цвет зелёный!");
+            }
+
+            else {
+                Console.BackgroundColor = ConsoleColor.Cyan;
+                Console.ForegroundColor = ConsoleColor.Black;   
+
+                Console.WriteLine("Ваш цвет бирюзовый!");
+            
+            }
+            Console.ReadKey();  
+        }
+    }
+}
